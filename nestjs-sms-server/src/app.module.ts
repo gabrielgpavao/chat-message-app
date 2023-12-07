40fdc12135aa3fde83d49faecc1e0d2f50ac9e85
gabriel <gabpavao@outlook.com>
2023-12-07 17:23:09 -0300

    feat: generate the Messages resource with Nest CLI

--- nestjs-sms-server/src/app.module.ts
+++ nestjs-sms-server/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service'
 import { UsersModule } from './resources/users/users.module'
 import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
+import { MessagesModule } from './resources/messages/messages.module'
 
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
         MongooseModule.forRoot(process.env.DATABASE_URL),
         UsersModule,
+        MessagesModule,
     ],
     controllers: [AppController],
     providers: [AppService],
