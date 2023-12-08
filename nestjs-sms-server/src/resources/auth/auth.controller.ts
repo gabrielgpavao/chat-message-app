888ab6e291bec5a93b09dba584767dc4009c0b13
gabriel <gabpavao@outlook.com>
2023-12-08 13:32:50 -0300

    feat: create a local strategy and its auth guard to validate users

--- nestjs-sms-server/src/resources/auth/auth.controller.ts
+++ nestjs-sms-server/src/resources/auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post, Body } from '@nestjs/common'
+import { Controller, Post, Body, UseGuards } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { LoginUserDto } from './dto/login.dto'
+import { LocalAuthGuard } from './guards/local-auth.guard'
 
-@Controller('auth')
+@Controller('login')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
+    @UseGuards(LocalAuthGuard)
     @Post()
     create(@Body() loginUserDto: LoginUserDto) {
         return this.authService.create(loginUserDto)
