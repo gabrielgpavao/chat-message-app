0830b7449b6d2ff5d63d0b948c0e09a1741422f8
gabriel <gabpavao@outlook.com>
2023-12-09 15:25:23 -0300

    style: global styles config

---
+++ react-chat-app/src/styles/GlobalStyles.ts
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+    html, body, div, span, applet, object, iframe,
+    h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+    a, abbr, acronym, address, big, cite, code,
+    del, dfn, em, img, ins, kbd, q, s, samp,
+    small, strike, strong, sub, sup, tt, var,
+    b, u, i, center, dl, dt, dd, ol, ul, li,
+    fieldset, form, label, legend, main,
+    table, caption, tbody, tfoot, thead, tr, th, td,
+    article, aside, canvas, details, embed, 
+    figure, figcaption, footer, header, hgroup, 
+    menu, nav, output, ruby, section, summary,
+    time, mark, audio, video, input, button, select, textarea {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 100%;
+        font: inherit;
+        vertical-align: baseline;
+        line-height: 1;
+        list-style: none;
+        box-sizing: border-box;
+        font-family: 'Inter', sans-serif;
+    }
+    
+    body{
+        background-color: #0D0630;
+        color: #F9F9F9;
+    }
+
+    a {
+        text-decoration: none;
+    }
+    
+    button {
+        cursor: pointer;
+    }
+
+    :root {
+        --brand-1: #E6F9AF;
+        --brand-2: #8BBEB2;
+
+        --blue-1: #0D0630;
+		--blue-2: #18314F;
+        --blue-3: #384E77;
+
+        --white: #F9F9F9;
+        
+        --error: #E63462
+    }
+`
