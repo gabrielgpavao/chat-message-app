e12fa8a29b6fd8dd3fdf988f8ae01ff27ddae071
gabriel <gabpavao@outlook.com>
2023-12-08 21:04:53 -0300

    feat: create a RedisService and register a CacheModule for AppModule

--- nestjs-sms-server/src/app.module.ts
+++ nestjs-sms-server/src/app.module.ts
@@ -1,15 +1,30 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { UsersModule } from './resources/users/users.module'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
 import { MessagesModule } from './resources/messages/messages.module'
 import { AuthModule } from './resources/auth/auth.module'
+import { CacheModule } from '@nestjs/cache-manager'
+import { redisStore } from 'cache-manager-redis-yet'
 
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
         MongooseModule.forRoot(process.env.DATABASE_URL),
+        CacheModule.registerAsync({
+            inject: [ConfigService],
+            useFactory: async (conf: ConfigService) => {
+                const config = {
+                    url: conf.get<string>('REDIS_URL'),
+                    ttl: conf.get<number>('REDIS_CACHE_TTL'),
+                }
+                return {
+                    store: await redisStore(config),
+                    ...config,
+                }
+            },
+        }),
         UsersModule,
         MessagesModule,
         AuthModule,
