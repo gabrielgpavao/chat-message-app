4ce408ec6390de4ad40f67ab2015b5a515d5b448
gabriel <gabpavao@outlook.com>
2023-12-11 17:34:24 -0300

    feat: apply conditional render to ChatGroups and SubscribeModal

--- react-chat-app/src/App.tsx
+++ react-chat-app/src/App.tsx
@@ -2,12 +2,16 @@ import { StyledContainer } from './styles/StyledContainer'
 import { ChatGroups } from './components/Chat'
 import { Toaster } from 'react-hot-toast'
 import { SubscribeModal } from './components/Modal/Subscribe'
+import { useState } from 'react'
 
 export function App() {
+	const [isSubscribeOpen, setIsSubscribeOpen] = useState(true)
+
 	return (
 		<StyledContainer>
-			<ChatGroups />
-			{/* <SubscribeModal /> */}
+			{isSubscribeOpen && <SubscribeModal setIsSubscribeOpen={setIsSubscribeOpen} />}
+			{!isSubscribeOpen && <ChatGroups />}
+
 			<Toaster position='top-right'/>
 		</StyledContainer>
 	)
