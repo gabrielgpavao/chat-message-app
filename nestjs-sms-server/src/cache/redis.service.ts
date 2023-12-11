cf5befa203d6c0465aa9b89333540a3beeba9f8d
gabriel <gabpavao@outlook.com>
2023-12-11 11:18:32 -0300

    fix: optimize the cache storage process in redis db

--- nestjs-sms-server/src/cache/redis.service.ts
+++ nestjs-sms-server/src/cache/redis.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { Redis } from 'ioredis'
+import { Schema } from 'mongoose'
 
 @Injectable()
 export class RedisService extends Redis {
@@ -23,5 +24,37 @@ export class RedisService extends Redis {
         const cachedMessages: string | null = await this.get(key)
 
         return JSON.parse(cachedMessages)
+    }
+
+    private messageCacheDatabaseName(
+        senderId: Schema.Types.ObjectId,
+        receiverId: Schema.Types.ObjectId,
+    ): string {
+        return 'message' + ':' + senderId + ':' + receiverId
+    }
+
+    async messageCacheDatabase(
+        senderId: Schema.Types.ObjectId,
+        receiverId: Schema.Types.ObjectId,
+    ): Promise<string | null> {
+        const firstOptionName = this.messageCacheDatabaseName(
+            senderId,
+            receiverId,
+        )
+
+        const firstOptionDatabase = await this.get(firstOptionName)
+
+        if (firstOptionDatabase) return firstOptionName
+
+        const secondOptionName = this.messageCacheDatabaseName(
+            receiverId,
+            senderId,
+        )
+
+        const secondOptionDatabase = await this.get(secondOptionName)
+
+        if (secondOptionDatabase) return secondOptionName
+
+        return this.messageCacheDatabaseName(senderId, receiverId)
     }
 }
